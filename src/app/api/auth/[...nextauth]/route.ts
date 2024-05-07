79a34fc14bae053bbc2ffb2aaf1199ccf181ac4e
Falvet Guillaume <guillaume.falvet@gmail.com>
2024-05-07 11:03:24 +0200

    ✨ feat : added typing

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,32 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import type { NextAuthOptions } from "next-auth"
 const whiteListedEmails = ["user1@example.com", "user2@example.com", "guillaume.falvet@gmail.com"];
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
-};
-export const handler = NextAuth(
-  {
-    ...authOptions,
-    callbacks: {
-      async signIn({ email, user }) {
-        console.log("signIn", { email });
-        if(!user.email) return false;
-        // Check if the email is in the list of allowed emails
-        // ! use db instead of hardcoded list
-        const isAllowedToSignIn = whiteListedEmails.includes(user.email);
-        if (isAllowedToSignIn) {
-          return true; // Continue with sign-in process
-        } else {
-          // Redirect to an unauthorized page or return false to display a default error message
-          return false;
-          // Or you can return a URL to redirect to:
-          // return '/unauthorized'
-        }
-      },
+  callbacks: {
+    async signIn({ email, user }) {
+      console.log("signIn", { email });
+      if(!user.email) return false;
+      // Check if the email is in the list of allowed emails
+      // ! use db instead of hardcoded list
+      const isAllowedToSignIn = whiteListedEmails.includes(user.email);
+      if (isAllowedToSignIn) {
+        return true; // Continue with sign-in process
+      } else {
+        // Redirect to an unauthorized page or return false to display a default error message
+        return false;
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
+      }
     },
-  }
-);
+  },
+};
+export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
