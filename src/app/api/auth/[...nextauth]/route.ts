13ea1ce70541d22cc28c1d8817e34b2b0c510743
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-05-19 14:24:05 +0200

    ♻️ refactor : authOption extraction

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import { prisma } from '../../../../lib/db';
-const authOptions: AuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
-  callbacks: {
-    async signIn({ user }) {
-      if (!user.email) return false;
-      const userExists = await prisma.user.findFirst({
-        where: {
-          email: user.email,
-        },
-      });
-      if (!userExists) {
-        return false;
-      }
-      return true;
-    },
-  },
-};
+import { authOptions } from "@/lib/authOption";
+import NextAuth from "next-auth/next";
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
