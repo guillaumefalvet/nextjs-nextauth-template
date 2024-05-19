63f6b4842b444926f63ced8d748642ec4c75ee1e
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-05-19 14:24:33 +0200

    ♻️ refactor : protected by middleware now

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,12 +1,6 @@
 'use client'
-import { redirect } from "next/navigation";
 import { useSession } from "next-auth/react";
 export default function Component() {
-  const { data: session, status } = useSession()
-
-  if (status === "authenticated") {
-    return <p>Signed in as {session?.user?.email}</p>
-  }
-
-  return redirect("/api/auth/signin");
+  const { data: session } = useSession()
+  return <p>Signed in as {session?.user?.email}</p>
 }
