cda6e18744b44eb8dd3fc3a82549358cf50d51e0
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-05-19 14:25:07 +0200

    🧹 remove : serverSession

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { getServerSession } from "next-auth";
 import SessionProvider from "../components/SessionProvider";
 import NavMenu from "../components/NavMenu";
 
@@ -19,12 +18,10 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>
+        <SessionProvider>
           <main className="mx-auto max-w-5xl text-2xl flex gap-2 text-white">
             <NavMenu />
             {children}
